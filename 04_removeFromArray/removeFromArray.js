b97af5254ab73ac598d1d3cb5407b9e7d46e4690
Josh R <jprennell20@gmail.com>
2022-08-01 19:34:07 -0400

    Add rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    
+    for(const arg of theArgs){
+        // Find index of argument, returns -1 if argument is not in array
+        let index = array.indexOf(arg);
+        if(index != -1){
+            array.splice(index,1);
+    }
+}
+    return array;
 };
 
 // Do not edit below this line
