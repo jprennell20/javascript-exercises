6abbbf38b5a37305c29b1be33c7cbf0423d15f19
Josh R <jprennell20@gmail.com>
2022-08-01 19:59:08 -0400

    Create loop and supporting logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+
+    // Confirm user provided integer arguments
+    if(Number.isInteger(firstInt) && firstInt >= 0 && 
+       Number.isInteger(secondInt) && secondInt >= 0){
+
+        // Identify the lower and upper boundaries
+        if(firstInt > secondInt){
+            upperBoundary = firstInt;
+            lowerBoundary = secondInt;
+        }else{
+            upperBoundary = secondInt;
+            lowerBoundary = firstInt;
+        }
+
+        // Loop from lower to upper boundary
+    
+        let result = 0;
+        for(let i = lowerBoundary; i <= upperBoundary; i++){
+            result += i;
+        }
+        return result;
+        
+    }else{
+        return "ERROR"
+    }
+
 
 };
 
