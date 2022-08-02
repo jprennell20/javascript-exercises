b5e6f3505c6770f3badc9d8623e321274af55d95
Josh R <jprennell20@gmail.com>
2022-08-01 21:06:57 -0400

    Create temp conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+function roundOneDecimal(num){
+  return Math.round(num*10) / 10;
+}
 
+const ftoc = function(temp) {
+  let convertedTemp = (temp - 32) * (5/9)
+  return roundOneDecimal(convertedTemp);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let convertedTemp = (temp * (9/5))+32
+  return roundOneDecimal(convertedTemp);
 };
 
 // Do not edit below this line
